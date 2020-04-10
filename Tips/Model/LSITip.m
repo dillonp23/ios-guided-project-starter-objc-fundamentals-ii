b73516d1159d6802240cdf24cbdb413a11fe621f
dillonp23 <dillonp.lambda@gmail.com>
2020-04-09 19:04:18 -0700

    Defined the initializer for the LSITip class

--- Tips/Model/LSITip.m
+++ Tips/Model/LSITip.m
@@ -10,4 +10,16 @@
 
 @implementation LSITip
 
+- (instancetype)initWithName:(NSString *)name total:(double)total splitCount:(NSInteger)splitCount tipPercentage:(double)tipPercentage
+
+{
+    if (self = [super init]) {
+        _name = name.copy; // if you want to access the instance property then you'll use the underscore
+        _total = total;
+        _splitCount = splitCount;
+        _tipPercentage = tipPercentage;
+    }
+    return self;
+}
+
 @end
