2ea180ad8ae49259715d1b6b9d5fc2bc948f91f0
dillonp23 <dillonp.lambda@gmail.com>
2020-04-09 18:58:07 -0700

    Declared an initializer for the LSITip class

--- Tips/Model/LSITip.h
+++ Tips/Model/LSITip.h
@@ -13,11 +13,17 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface LSITip : NSObject
 
+// can only put copy keyword on something that confomrs to the asCopy protocol
+
 @property (nonatomic, readonly, copy) NSString *name; // copy will make sure whatever is saved inside model object, its not version of string that can be changed by something else - due to mutability
 @property (nonatomic, readonly) double total; // no star because not an object but its a primitive value
 @property (nonatomic, readonly) NSInteger splitCount; // same here
 @property (nonatomic, readonly) double tipPercentage;
 
+- (instancetype)initWithName:(NSString *)name
+                       total:(double)total
+                  splitCount:(NSInteger)splitCount
+               tipPercentage:(double)tipPercentage;
 
 @end
 
